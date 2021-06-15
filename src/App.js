0d3f79d24e1e7fd95bdd0721932b6d0588250b79
John Manack <john.manack@semanticbits.com>
2021-06-15 16:25:05 -0400

    initial commit; includes basic file structure and data

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const data = [
+  { name: "John", gender: "male", math: 100, physics: 80 },
+  { name: "Jane", gender: "female", math: 80, physics: 20 },
+  { name: "Biju", gender: "male", math: 60, physics: 90 },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chart App</h1>
+    </div>
+  );
+}
+
+export default App;
